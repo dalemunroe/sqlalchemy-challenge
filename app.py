0f3651e005e8784fc878428b9f15a96c5665d993
dale munroe <dale.munroe@geochelle.com.au>
2022-09-06 23:32:53 +0800

    Q2 Completed and working

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-
 from os import stat
 from sqlalchemy import distinct
 import numpy as np
@@ -49,8 +48,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end><br/>"
+        #f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
 
 # Convert the query results to a dictionary using date as the key and prcp as the value
@@ -58,17 +57,18 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
-    st_query_date = dt.date(2017, 8, 23) - dt.timedelta(days=364)
+    st_query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
     sel = [Measurement.date,
            func.sum(Measurement.prcp)]
-    last_years_data = session.query(*sel).\
+    last_years_prcp_data = session.query(*sel).\
         filter(Measurement.date > st_query_date).\
         group_by(Measurement.date).\
-        order_by(Measurement.date).all()   
-    prcp_dict = {date: prcp for date, prcp in last_years_data}
+        order_by(Measurement.date).all() 
+    session.close()      
+    prcp_dict = {date: prcp for date, prcp in last_years_prcp_data}
 
-    return jsonify(prcp_dict)
+    return jsonify(Last_years_prcp_data=prcp_dict)
 
 # Return a JSON list of stations from the dataset.
 
@@ -78,8 +78,8 @@ def stations():
     no_stations = session.query(Station.station).all()
     
     session.close()
-    stations=list(np.ravel(no_stations))
-    return jsonify(stations=stations)
+    number_of_stations=list(np.ravel(no_stations))
+    return jsonify(Number_of_stations=number_of_stations)
 
 
 # Query the dates and temperature observations of the most active station for the previous year of data.
@@ -87,13 +87,14 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def temp():
     st_query_date = dt.date(2017, 8, 23) - dt.timedelta(days=364)
-    station_tobs_data = session.query(Measurement.date, Measurement.tobs).\
+    mostact_station_tobs_data = session.query(Measurement.date, Measurement.tobs).\
         filter(Measurement.date > st_query_date).\
         filter(Measurement.station == "USC00519281").\
         order_by(Measurement.date).all()   
     session.close()
-    temps=list(np.ravel(station_tobs_data))
-    return jsonify(temps)
+    station_tobs_data=list(np.ravel(mostact_station_tobs_data))
+    
+    return jsonify(Station_tobs_data=station_tobs_data)
 
 # Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a given start or start-end range.
 # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than or equal to the start date.
@@ -103,25 +104,27 @@ def temp():
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
 def stats(start=None, end=None):
-    sel = [Measurement.tobs,
-           func.max(Measurement.tobs),
+    sel = [func.max(Measurement.tobs),
            func.min(Measurement.tobs),
            func.avg(Measurement.tobs)]
 
     if not end:
         start = dt.datetime.strptime(start, "%m%d%Y")
-        most_active = session.query(*sel).\
+        tempstat_to_ds_end = session.query(*sel).\
             filter(Measurement.date >= start).all()
         session.close()
-        return jsonify(most_active)
+        tempstat_result=list(np.ravel(tempstat_to_ds_end))
+        return jsonify(Temp_to_end = tempstat_result)
 
     start = dt.datetime.strptime(start, "%m%d%Y")
     end = dt.datetime.strptime(end, "%m%d%Y")
-    most_active = session.query(*sel).\
-        filter(Measurement.date >= start and end).all()
+    tempstat_to_nom_end = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
     session.close()
-    statss=list(np.ravel(stat))
-    return jsonify(statss)
+    tempstat_result_range=list(np.ravel(tempstat_to_nom_end))
+    return jsonify(Temp_nom_range=tempstat_result_range)
+
 
 if __name__ == '__main__':
     app.run()
